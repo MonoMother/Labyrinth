e57b3fbc25f9b48c16fa9a3cb8f38616226b161e
MonoMother <92880982+MonoMother@users.noreply.github.com>
2023-07-11 03:32:18 +0400

    v2
    Database entry implemented

--- labyrinth/src/main/java/org/example/Main.java
+++ labyrinth/src/main/java/org/example/Main.java
@@ -1,6 +1,24 @@
 package org.example;
-public class Main {
+
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+    public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //путь к БД
+        final String DB_URL = "jdbc:mysql://localhost:3306/labyrinthdatabase";
+
+        System.out.println("Введите имя пользователя MySQL: ");
+        String USERNAME = scanner.nextLine();
+
+        System.out.println("Введите пароль: ");
+        String PASSWORD = scanner.nextLine();
+
         //количество столбцов и строк в лабиринте
         int lines = 10, columns = 10;
         //процент разреженности лабиринта
@@ -13,11 +31,49 @@ public class Main {
         labyrinth.DFSearch(arr, lines, columns);
         labyrinth.printLabyrinth(arr);
 
+
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD)) {
+            // Создаем подготовленное выражение для вставки данных в таблицу
+            String sql = "INSERT INTO mytable (value) VALUES (?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            // Проходимся по всем элементам массива и записываем их в базу данных
+            for (int i = 0; i<lines; i++){
+                for (int j = 0; j<columns; j++){
+                    statement.setString(1, String.valueOf(arr[i][j]));
+                    statement.executeUpdate();
+                }
+            }
+
+            System.out.println("Данные прохождения лабиринта DF поиском успешно записаны в базу данных.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         labyrinth.cleanLabyrinth(arr);
 
         labyrinth.BFSearch(arr, lines, columns);
         labyrinth.printLabyrinth(arr);
 
+        try (Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD)) {
+            // Создаем подготовленное выражение для вставки данных в таблицу
+            String sql = "INSERT INTO mytable (value) VALUES (?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
 
+            // Проходимся по всем элементам массива и записываем их в базу данных
+            for (int i = 0; i<lines; i++){
+                for (int j = 0; j<columns; j++){
+                    statement.setString(1, String.valueOf(arr[i][j]));
+                    statement.executeUpdate();
+                }
+            }
+
+            System.out.println("Данные прохождения лабиринта BF поиском успешно записаны в базу данных.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     }
